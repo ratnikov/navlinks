8dbda4766ce363c86f1b05f759283eca5355918c
ratnikov <ratnikov@gmail.com>
2010-01-25 19:26:38 -0600

    Added the initial commit with partial README

---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,21 @@
+# Methods added to this helper will be available to all templates in the application.
+module NavigationHelper
+  attr_accessor :nav_area
+
+  def nav_link_to area, path, options = nil
+    label = translate(area, :scope => 'navigation', :default => area.to_s)
+    current_label = translate([ area, 'current' ].join('_'), :scope => 'navigation', :default => label)
+
+    if nav_area == area
+      link_to(decorate_current(current_label), path, :class => 'current')
+    else
+      link_to(label, path)
+    end
+  end
+
+  private
+
+  def decorate_current( current_label )
+    "[ #{current_label} ]"
+  end
+end
